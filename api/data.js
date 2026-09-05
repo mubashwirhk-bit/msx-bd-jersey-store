443ed7de6cce0ba7a8a591ae183cd4153fa1eb79
mubashwirhk-bit <mubashwirhk@gmail.com>
2026-09-05 15:59:17 +0600

    Add sample jersey data

---
+++ api/data.js
@@ -0,0 +1,48 @@
+// Sample Jersey Data
+export const jerseyData = [
+  {
+    id: 1,
+    team: 'Barcelona',
+    type: 'Club',
+    color: 'Blue & Red',
+    size: 'M',
+    price: 3500,
+    description: 'Official Barcelona FC Home Jersey - Season 2024'
+  },
+  {
+    id: 2,
+    team: 'Bangladesh National Team',
+    type: 'National',
+    color: 'Green & Red',
+    size: 'L',
+    price: 2500,
+    description: 'Bangladesh Cricket Team Jersey'
+  },
+  {
+    id: 3,
+    team: 'Manchester United',
+    type: 'Club',
+    color: 'Red',
+    size: 'M',
+    price: 3800,
+    description: 'Manchester United Home Jersey - Premium Edition'
+  },
+  {
+    id: 4,
+    team: 'Real Madrid',
+    type: 'Club',
+    color: 'White',
+    size: 'L',
+    price: 4000,
+    description: 'Real Madrid Home Jersey - 2024 Season'
+  },
+  {
+    id: 5,
+    team: 'Juventus',
+    type: 'Club',
+    color: 'Black & White',
+    size: 'S',
+    price: 3200,
+    description: 'Juventus Home Jersey - Classic Stripes'
+  }
+];
